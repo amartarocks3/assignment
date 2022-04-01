a2b8bd9fce765a7ae211cc36aa6ed2d88aa1c7ef
amartarocks3 <amartarocks3@gmail.com>
2022-04-01 09:33:40 +0530

    Updated : vovel

--- src/assignment2/operator1.java
+++ src/assignment2/operator1.java
@@ -4,7 +4,41 @@ import java.util.Scanner;
 
 public class operator1 {
 	public static void main(String[] args) {
+		int sum1=0,sum2=0,sum3=0,sum4=0;
 		Scanner sc = new Scanner(System.in);
-		int a
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sum1=a+b*c;
+		sum2=c+a/b;
+		sum3=(a%b)+c;
+		sum4=a*b+c;
+		if(sum1>sum2 && sum1>sum3 && sum1>sum4) {
+			System.out.println("sum1 is max " + sum1);
+		}
+		else if(sum2>sum1 && sum2>sum3 && sum2>sum4) {
+			System.out.println("sum2 is max " + sum2);
+		}
+		else if(sum3>sum1 && sum3>sum2 && sum3>sum4) {
+			System.out.println("sum3 is max " + sum3);
+		}
+		else {
+			System.out.println("sum4 is max " + sum4);
+		}
+		
+		if(sum1<sum2 && sum1<sum3 && sum1<sum4) {
+			System.out.println("sum1 is min " + sum1);
+		}
+		else if(sum2<sum1 && sum2<sum3 && sum2<sum4) {
+			System.out.println("sum2 is min " + sum2);
+		}
+		else if(sum3<sum1 && sum3<sum2 && sum3<sum4) {
+			System.out.println("sum3 is min " + sum3);
+		}
+		else {
+			System.out.println("sum4 is min " + sum4);
+		}
+		
+		
 	}
 }
--- src/assignment2/vovelcons.java
+++ src/assignment2/vovelcons.java
@@ -7,9 +7,10 @@ public class vovelcons {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a character");
 		char c = sc.next().charAt(0);
-		if( c=="a" || c=="e" || c=="i" || c=="o" || c=="u") {
-			System.out.println(c + "is vovel");
+		if( c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+			System.out.println(c + " is vovel");
 		}
 		else
 		{
